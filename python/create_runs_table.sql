7bc45506c3a4d575c0b4326f620fc0c80d6dbacf
Ashish Agarwal <agarwal1975@gmail.com>
2011-10-13 16:49:07 -0400

    Removed old Python/Django based implementation.

--- python/create_runs_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE runs (
-  run_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (run_id),
-  member_id INT UNSIGNED NOT NULL,
-  FOREIGN KEY (member_id) REFERENCES members (member_id)
-) ENGINE = InnoDB;
--- python/make_member_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE member (
-  first_name VARCHAR(20) NOT NULL,
-  last_name VARCHAR(20) NOT NULL,
-  member_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (member_id)
-) ENGINE = InnoDB;
