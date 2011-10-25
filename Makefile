1dbdefa10c330f7b629cf528fea8f8c6ae07cf83
Sebastien Mondet <seb@mondet.org>
2011-10-25 12:38:46 -0400

    Enable static linking of the ocsigen server
    I added a `static' make target and a lunch script working on the
    `hitscoreserver' binary.

---
+++ Makefile
@@ -0,0 +1,30 @@
+all: build
+
+build:
+	ocaml setup.ml -build
+
+static: install
+	ocamlfind ocamlopt -linkall \
+	    -package ocsigenserver,ocsigenserver.ext.ocsipersist-sqlite  \
+	    -package eliom.server,ocsigenserver.ext.staticmod  \
+	    -package batteries \
+	    -package hitscoreweb \
+            server_main.cmx -o hitscoreserver -linkpkg -thread
+
+install: build
+	ocaml setup.ml -reinstall
+
+uninstall:
+	ocaml setup.ml -uninstall
+
+doc:
+	ocaml setup.ml -doc
+
+clean:
+	ocaml setup.ml -clean
+
+
+# clean everything and uninstall
+fresh: clean uninstall
+
+.PHONY: doc install uninstall clean fresh
--- src/lib/Hitscore_web.ml
+++ src/lib/Hitscore_web.ml
@@ -3,12 +3,24 @@ open Hitscore_web_std
 
 
 
-let default = Eliom_output.Html5.register_service [""] Eliom_parameters.unit
-  (fun () () ->
-    Lwt.return
-     (let open HTML5.M in
-      html
-        (head (title (pcdata "Hitscore Web")) [])
-        (body [p [pcdata "Histcore default web page."]])))
-  
+let default () =
+  let default =
+    Eliom_services.service
+      ~path:[""] ~get_params:Eliom_parameters.unit () in
+  Eliom_output.Html5.register ~service:default
+    (fun () () ->
+      eprintf "Registering default\n%!";
+      Lwt.return
+        (let open HTML5.M in
+         html
+           (head (title (pcdata "Hitscore Web")) [])
+           (body [p [pcdata "Histcore default web page."]])))
+
+let () =
+
+  Eliom_services.register_eliom_module
+    "hitscoreweb" 
+    (fun () ->
+      default ()
+    )
 
