57c9814b49f409a9f571fb31011b71b612b86b4d
Sebastien Mondet <seb@mondet.org>
2012-02-20 14:55:00 -0500

    Fix build dependencies (again)

--- bin/fake_eliomdep.ml
+++ bin/fake_eliomdep.ml
@@ -6,12 +6,13 @@ let () =
     function
     | [] -> ()
     | h :: t ->
-      let deps =
-        (String.concat " " (List.map (sprintf "_server/%s.cmi") t)) in
-      printf "_server/%s.cmo: %s\n" h deps;
-      printf "_server/%s.cmx: %s\n" h deps;
-      printf "_client/%s.cmo: %s\n" h deps;
-      printf "_client/%s.cmx: %s\n" h deps;
+      let deps where =
+        (String.concat " " (List.rev_map (sprintf "_%s/%s.cmi" where) t)) in
+      printf "_server/%s.cmo: %s\n" h (deps "server");   
+      printf "_server/%s.cmx: %s\n" h (deps "server");   
+      printf "_client/%s.cmo: %s\n" h (deps "client");   
+      printf "_client/%s.cmx: %s\n" h (deps "client"); 
+      printf "_client/%s.cmi: %s.type_mli\n" h h;  
       go_through t
   in
   go_through modules
