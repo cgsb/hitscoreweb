0bb1be4eab562fa2198d487e64b0ef2a46b49962
Sebastien Mondet <seb@mondet.org>
2012-04-25 11:14:34 -0400

    Improve build system

--- bin/fake_eliomdep.ml
+++ bin/fake_eliomdep.ml
@@ -6,12 +6,13 @@ let () =
     function
     | [] -> ()
     | h :: t ->
-      let deps where =
-        (String.concat " " (List.rev_map (sprintf "_%s/%s.cmi" where) t)) in
-      printf "_server/%s.cmo: %s\n" h (deps "server");   
-      printf "_server/%s.cmx: %s\n" h (deps "server");   
-      printf "_client/%s.cmo: %s\n" h (deps "client");   
-      printf "_client/%s.cmx: %s\n" h (deps "client"); 
+      let deps where what =
+        (String.concat " " (List.rev_map (fun s ->
+          sprintf "_%s/%s.%s" where s what) t)) in
+      printf "_server/%s.cmo: %s\n" h (deps "server" "cmo");
+      printf "_server/%s.cmx: %s\n" h (deps "server" "cmx");   
+      printf "_client/%s.cmo: %s\n" h (deps "client" "cmo");   
+      printf "_client/%s.cmx: %s\n" h (deps "client" "cmx"); 
       printf "_client/%s.cmi: %s.type_mli\n" h h;  
       go_through t
   in
