7000339201e050542838830c37eae96da7b0b201
Sebastien Mondet <seb@mondet.org>
2013-08-01 16:10:14 -0400

    /libraries: display all deliveries for users
    This fixes agarwal/hitscore#99.

--- src/webapp/hitscoreweb_libraries.ml
+++ src/webapp/hitscoreweb_libraries.ml
@@ -129,52 +129,69 @@ let detailed_fastq_subtable lib =
   `subtable (if List.concat subtable = [] then empty_row else subtable)
 
 
-let simple_fastq_subtable lib =
+let simple_fastq_subtable ~classy_cache ~user_opt lib =
   let open Template in
   let open Html5 in
   let module Bui = Hitscore_interfaces.B2F_unaligned_information in
   let subtable =
     List.map lib#submissions (fun sub ->
-      List.map sub#flowcell#hiseq_raws (fun hs ->
-        List.filter_map hs#demultiplexings (fun dmux ->
-          let open Option in
-          let fastq_stats = Data_access.get_fastq_stats lib sub dmux in
-          let fcid = sub#flowcell#oo#serial_name in
-          Data_access.choose_delivery_for_user dmux sub
-          >>= fun (del, inv) ->
-          return [
-            `sortable (sprintf "%s:%d" fcid sub#lane_index,
-                       [a_link Services.flowcell [pcdata fcid] fcid;
-                        pcdataf " (Lane %d)"  sub#lane_index; ]);
-            `text [
-              Option.(
-                let default = "" in
-                pcdataf " %s (mismatch: %d) %s (%s)"
-                  (value_map dmux#b2f#tiles ~default ~f:(sprintf "(tiles:%s)"))
-                  dmux#b2f#mismatch
-                  (value_map dmux#b2f#bases_mask
-                     ~default ~f:(sprintf "(bmask:%s)"))
-                  (value ~default:"unknown-sample-sheet-kind"
-                     (dmux#assembly >>= fun a ->
-                      return a#kind
-                      >>| Layout.Enumeration_sample_sheet_kind.to_string)));
-            ];
-            `text Option.(
-              value_map fastq_stats ~default:[pcdata "—"]
-                ~f:(fun s ->
-                  [codef "%s"
-                      (pretty_string_of_float ~sof:(sprintf "%.0f")
-                         s.Bui.cluster_count)]));
-            `text Option.(
-              value_map fastq_stats ~default:[pcdata "—"]
-                ~f:(fun s ->
-                  [codef "%.2f%%" (100. *. s.Bui.yield_q30 /. s.Bui.yield) ]));
-            `text Option.(
-              value_map fastq_stats ~default:[pcdata "—"]
-                ~f:(fun s ->
-                  [codef "%.2f" (s.Bui.quality_score_sum /. s.Bui.yield)]));
-          ]) |> List.concat) |> List.concat) in
-  let empty_row = [List.init 5 (fun _ -> `text [pcdata ""])] in
+        List.map sub#flowcell#hiseq_raws (fun hs ->
+            List.filter_map hs#demultiplexings (fun dmux ->
+                let open Option in
+                let fastq_stats = Data_access.get_fastq_stats lib sub dmux in
+                let fcid = sub#flowcell#oo#serial_name in
+                let deliveries = dmux#deliveries in
+                some_if (deliveries <> []) (
+                  List.filter_map deliveries (fun del ->
+                      let delivery_in_meta_hiseq_runs =
+                        List.find_map classy_cache#meta_hiseq_runs (fun mhr ->
+                            List.find mhr#deliveries (fun dd ->
+                                dd#delivery#oo#g_id = del#oo#g_id))
+                      in
+                      delivery_in_meta_hiseq_runs
+                      >>= fun deliv ->
+                      let filtered_lanes =
+                        List.filter deliv#lanes (fun l ->
+                            let contacts = Array.to_list l#lane#contacts in
+                            List.exists contacts (fun c -> Some c#pointer = user_opt))
+                      in
+                      some_if (filtered_lanes <> []) [
+                        `sortable (sprintf "%s:%d" fcid sub#lane_index,
+                                   [a_link Services.flowcell [pcdata fcid] fcid;
+                                    pcdataf " (Lane %d)"  sub#lane_index; ]);
+                        `text [
+                          Option.(
+                            let default = "" in
+                            pcdataf " %s (mismatch: %d) %s (%s)"
+                              (value_map dmux#b2f#tiles ~default ~f:(sprintf "(tiles:%s)"))
+                              dmux#b2f#mismatch
+                              (value_map dmux#b2f#bases_mask
+                                 ~default ~f:(sprintf "(bmask:%s)"))
+                              (value ~default:"unknown-sample-sheet-kind"
+                                 (dmux#assembly >>= fun a ->
+                                  return a#kind
+                                  >>| Layout.Enumeration_sample_sheet_kind.to_string)));
+                        ];
+                        `text [pcdata "In ";
+                               codef "%s" (value_map del#client_fastqs_dir
+                                             ~default:"???"
+                                             ~f:(fun c -> c#directory))];
+                        `text Option.(
+                            value_map fastq_stats ~default:[pcdata "—"]
+                              ~f:(fun s ->
+                                  [codef "%s"
+                                     (pretty_string_of_float ~sof:(sprintf "%.0f")
+                                        s.Bui.cluster_count)]));
+                        `text Option.(
+                            value_map fastq_stats ~default:[pcdata "—"]
+                              ~f:(fun s ->
+                                  [codef "%.2f%%" (100. *. s.Bui.yield_q30 /. s.Bui.yield) ]));
+                        `text Option.(
+                            value_map fastq_stats ~default:[pcdata "—"]
+                              ~f:(fun s ->
+                                  [codef "%.2f" (s.Bui.quality_score_sum /. s.Bui.yield)]));
+                      ])))) |> List.concat) |> List.concat |> List.concat in
+  let empty_row = [List.init 6 (fun _ -> `text [pcdata ""])] in
   `subtable (if List.concat subtable = [] then empty_row else subtable)
 
 let make_file_links vol_id paths =
@@ -192,7 +209,7 @@ let make_file_links vol_id paths =
   end
   |> interleave_list ~sep:(pcdata ", ")
 
-let libraries_table ~showing ~can_view_fastq_details info =
+let libraries_table ~showing ~can_view_fastq_details ~classy_cache ~user_opt info =
   let open Template in
   let open Html5 in
   let everywhere f = ([`basic; `stock; `fastq], f) in
@@ -298,7 +315,7 @@ let libraries_table ~showing ~can_view_fastq_details info =
         if can_view_fastq_details then
           detailed_fastq_subtable lib
         else
-          simple_fastq_subtable lib)
+          simple_fastq_subtable ~classy_cache ~user_opt lib)
       ]) in
   let first_row =
     let fastq_part =
@@ -310,6 +327,7 @@ let libraries_table ~showing ~can_view_fastq_details info =
          fastq (fun () -> `head_cell Msg.mean_qs); ]
       else
         [fastq (fun () -> `head_cell Msg.demux_options);
+         fastq (fun () -> `head [pcdata "Delivery"]);
          fastq (fun () -> `head_cell Msg.number_of_reads);
          fastq (fun () -> `head_cell Msg.percent_bases_over_q30);
          fastq (fun () -> `head_cell Msg.mean_qs); ]
@@ -663,6 +681,11 @@ let libraries ~showing work_started info_got info =
      else per_lirbary_simple_details info
    else return (Template.content_list []))
   >>= fun details ->
+  Web_data_access.classy_cache ()
+  >>= fun classy_cache ->
+  Authentication.user_logged ()
+  >>| Option.map ~f:(fun ul -> ul.Authentication.person)
+  >>= fun user_opt ->
   let libnb = List.length info#libraries in
   return Template.(
     content_section (pcdataf "Viewing %d Librar%s" libnb
@@ -672,7 +695,7 @@ let libraries ~showing work_started info_got info =
         content_section (pcdataf "Summary Table")
           (content_list [
             content_paragraph (intro_paragraph info);
-            libraries_table ~showing ~can_view_fastq_details;
+            libraries_table ~classy_cache ~user_opt ~showing ~can_view_fastq_details;
            ]);
         details;
       ]))
